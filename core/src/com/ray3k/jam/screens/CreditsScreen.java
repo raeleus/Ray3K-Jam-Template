4954c4b78b1ed005f7baae511d30eb60ece13929
Raymond Buckley <raymond.ray3k@gmail.com>
2019-02-16 07:21:19 -0800

    Added credits screen.

--- core/src/com/ray3k/jam/screens/CreditsScreen.java
+++ core/src/com/ray3k/jam/screens/CreditsScreen.java
@@ -24,26 +24,43 @@
 package com.ray3k.jam.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ray3k.jam.Core;
+import com.ray3k.jam.EntityManager;
 import com.ray3k.jam.JamScreen;
+import com.ray3k.jam.entities.credits.CreditsEntity;
 
 /**
  *
  * @author Raymond
  */
 public class CreditsScreen extends JamScreen {
-
-    public CreditsScreen(Core core) {
-
-    }
+    private Core core;
+    private EntityManager entityManager;
+    private Viewport viewport;
     
+    public CreditsScreen(final Core core) {
+        this.core = core;
+        entityManager = new EntityManager();
+        viewport = new ScreenViewport();
+        viewport.update(800, 800, true);
+        viewport.apply();
+        
+        var credits = new CreditsEntity(core);
+        entityManager.addEntity(credits);
+    }
+
     @Override
     public void show() {
+        Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand);
     }
 
     @Override
     public void act(float delta) {
+        entityManager.act(delta);
     }
 
     @Override
@@ -51,10 +68,16 @@ public class CreditsScreen extends JamScreen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
+        core.batch.setProjectionMatrix(viewport.getCamera().combined);
+        core.batch.begin();
+        core.batch.setBlendFunction(GL20.GL_ONE, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        entityManager.draw(core.batch, delta);
+        core.batch.end();
     }
-    
+
     @Override
     public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -67,10 +90,13 @@ public class CreditsScreen extends JamScreen {
 
     @Override
     public void hide() {
+        Gdx.input.setInputProcessor(null);
+        
+        Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
     }
 
     @Override
     public void dispose() {
+        
     }
-   
 }
